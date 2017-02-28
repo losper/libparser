279d87777d9104c06ae0e580b034f9a6ec6d6117
liuwenjun <liuwenjun@hangsheng.com.cn>
2017-02-28 11:06:48 +0000

    fixed for arm-linux

--- cmake/imx6q.cmake
+++ cmake/imx6q.cmake
@@ -3,7 +3,7 @@ SET(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER /home/lwj/dev/bin/arm-linux-gnueabihf-g++)
+SET(CMAKE_C_COMPILER /home/lwj/dev/bin/arm-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER /home/lwj/dev/bin/arm-linux-gnueabihf-g++)
 
 # where is the target environment 
