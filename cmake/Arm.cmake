5177b6be887a55711efd62600608c391af6f4cf4
liuwenjun <260251708@qq.com>
2017-02-28 10:12:22 +0800

    适配arm-linux

---
+++ cmake/Arm.cmake
@@ -0,0 +1,19 @@
+# this is required
+SET(CMAKE_SYSTEM_NAME Linux)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   /home/lwj/dev/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /home/lwj/dev/bin/arm-linux-gnueabihf-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  /home/lwj/dev/arm-linux-gnueabihf)
+
+# search for programs in the build host directories (not necessary)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# configure Boost and Qt
+#SET(QT_QMAKE_EXECUTABLE /opt/qt-embedded/qmake)
+#SET(BOOST_ROOT /opt/boost_arm)
