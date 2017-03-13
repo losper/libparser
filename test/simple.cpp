1e5daf441ab044b8733c7d1d7d5a9cf2158c7756
liuwenjun <liuwenjun@hangsheng.com.cn>
2017-03-13 14:28:28 +0800

    reflash

--- test/simple.cpp
+++ test/simple.cpp
@@ -1,11 +1,29 @@
-#include "../src/capture.h"
-#include "../src/libcapture.h"
 #include "windows.h"
+#include <map>
+#include <list>
+#include <string>
+enum json_type {
+	enum_string,
+	enum_number,
+	enum_array,
+	enum_object,
+	enum_null,
+};
+class json_object {
+private:
+	json_type type;
+	std::map<std::string, json_object> sjo;
+};
 
+json_object json_parser(const char* buf) {
+
+}
 int main() {
-	captureLink();
-	captureStart();
-	//Sleep(5000);
-	//captureStop();
+	char* buf = "{'imx':{'name':'liuwenjun','sex':'male','class':'3-2','age':20},'alist':[1,2,3,4,5],'blist':['123','e45','543']}";
+	json_t obj=json_parser(buf);
+	char* name=json_getstring(obj,"imx.name");
+	int value = json_getint(obj,"imx.age");
+	json_getlist(obj,"alist");
+
 	return 0;
 }
