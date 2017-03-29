4f4260c40f0564ce6f2df7517c42c1a2ec93c043
liuwenjun <liuwenjun@hangsheng.com.cn>
2017-03-29 17:38:29 +0800

    增加文档，并配置忽略文件

---
+++ buildimx6q.sh
@@ -0,0 +1,6 @@
+rm -rf imx6qbuild
+mkdir imx6qbuild
+cd imx6qbuild
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/imx6q.cmake -DCMAKE_INSTALL_PREFIX=~/dev/arm-linux-gnueabihf
+#cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+#cmake --build ./
