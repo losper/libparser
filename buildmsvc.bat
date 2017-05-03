c1a6cbf9440cad388735402eb3e8cc8516bbb4aa
liuwenjun <liuwenjun@hangsheng.com.cn>
2017-05-03 20:45:05 +0800

    fixed buildmsvc&msvc

--- buildmsvc.bat
+++ buildmsvc.bat
@@ -1,10 +1,21 @@
 echo "generate this project"
-
+echo on
 rd msvcbuild /S/Q
 mkdir msvcbuild
 cd msvcbuild
+rem cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
 
 set GENERATOR=Visual Studio 14 2015 Win64
 set CONFIG=Release
-cmake ../ -G "%GENERATOR%"
+if '%1'=='' goto :yes
+goto :no
+:yes
+cmake ../ -G "%GENERATOR%" -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.cmake
+cmake --build ./ --target INSTALL
 pause
+goto :eof
+
+:no
+cmake ../ -G %1 %3 %4 %5
+if %2==1 cmake --build ./ --target INSTALL
+if %errorlevel% equ 0 goto :eof
