fd449d0fd3e5c0a756e4f96947078626a531f539
liuwenjun <liuwenjun@hangsheng.com.cn>
2017-05-08 15:20:31 +0800

    auto commit

--- cmake/remote248.cmake
+++ cmake/remote248.cmake
@@ -1,6 +1,25 @@
+# this is required
+SET(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER /opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER /opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  /opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/)
+SET(CMAKE_EXT_LIBRARAY_PATH  /home/hs/workspace/build/tmp/sysroots/mx6q/usr)
+# search for programs in the build host directories (not necessary)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# configure Boost and Qt
+#SET(QT_QMAKE_EXECUTABLE /opt/qt-embedded/qmake)
+#SET(BOOST_ROOT /opt/boost_arm)
+
 if(CMAKE_INCLUDE_PREFIX)
-else()
-set(CMAKE_INCLUDE_PREFIX e:/usr/local)
-endif()
 include_directories(${CMAKE_INCLUDE_PREFIX}/include)
 LINK_DIRECTORIES (${CMAKE_INCLUDE_PREFIX}/lib)
+endif()
